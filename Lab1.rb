72b7f7332260b063016d855d7781ef5c75cbce63
VeraSkokova <miss.smiley17@gmail.com>
2019-09-15 16:10:44 +0700

    Lab1

---
+++ Lab1.rb
@@ -0,0 +1,10 @@
+def string_permutation(array, n)
+	if n == 1
+		return array.select{|s| s==s}
+	end
+	string_permutation(array, n-1).map{|prefix| array.reduce([]){|strings, symbol| strings.push(prefix + symbol)}}
+											.flatten
+											.reject{|arr| arr[-1] == arr[-2]}
+end
+
+#puts string_permutation(%w(a b c), 3)
