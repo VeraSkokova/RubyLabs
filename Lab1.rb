d13ce72e3e562974f458a27ba63660bc2652ea1a
VeraSkokova <miss.smiley17@gmail.com>
2019-09-17 17:16:17 +0700

    Refactoring

--- Lab1.rb
+++ Lab1.rb
@@ -1,8 +1,8 @@
 def string_permutation(array, n)
   if n == 1
-    return array.select { |s| s == s }
+    return array
   end
-  string_permutation(array, n - 1).map { |prefix| array.reduce([]) { |strings, symbol| strings.push(prefix + symbol) } }
+  string_permutation(array, n - 1).map { |prefix| array.reduce([]) { |permutations, symbol| permutations.push(prefix + symbol) } }
       .flatten
       .reject { |arr| arr[-1] == arr[-2] }
 end
